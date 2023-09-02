ce040d05c9712add9304f7f19f35ebb5746677d2
Sabin Shrestha <53925686+sabin411@users.noreply.github.com>
2023-09-02 23:12:51 +0545

    gauri ko change commit.

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const Container = styled.div`
 function App() {
   return (
     <Container>
-      <p>New changes.</p>
+      <p>New changes. gauri ko change......</p>
       <button css={button_css}>I'm inline css</button>
       <StyledButton>I'm styled component</StyledButton>
     </Container>
